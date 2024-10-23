1de33aefa7a9230d68de764991b54df2d86c4c0d
takaryo1010 <t.ryota1010@gmail.com>
2024-10-24 01:32:09 +0900

    fix:バリデーション

--- validator/food_validator.go
+++ validator/food_validator.go
@@ -2,6 +2,7 @@ package validator
 
 import (
 	"RefrigeratorWatchdog-server/model"
+	"time"
 
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 )
@@ -25,9 +26,20 @@ func (fv *foodValidator) ValidateFood(food model.Food) error {
 		validation.Field(&food.UserID, validation.Required),
 		validation.Field(&food.OriginalCode,  validation.Min(0), validation.Max(10000000000000)),
 		validation.Field(&food.Quantity, validation.Min(0.0), validation.Max(10000000000000.0)),
-		validation.Field(&food.ExpirationDate),
+		validation.Field(&food.ExpirationDate, validation.By(allowNilTime)),
 		validation.Field(&food.ImageURL,  validation.Length(0, 10000)),
 		validation.Field(&food.Memo, validation.Length(0, 1000)),
 		validation.Field(&food.Tag, validation.In("野菜", "肉", "魚", "乳製品", "調味料", "卵", "飲料", "果物", "加工食品", "その他","")),
 	)
+}
+
+func allowNilTime(value interface{}) error {
+    if value == nil {
+        return nil
+    }
+    _, ok := value.(*time.Time)
+    if !ok {
+        return validation.NewError("validation_invalid", "無効な日付フォーマットです")
+    }
+    return nil
 }
